335cfc8e094f624d67effc83834f9ca7e4736f6e
Mitch Kahn <makahn@amazon.com>
2022-07-20 18:00:23 -0700

    added flexbox demo

--- src/LandingScreen.tsx
+++ src/LandingScreen.tsx
@@ -5,6 +5,12 @@ import { observer } from 'mobx-react-lite';
 import { useApplicationState } from './state/MSTProvider';
 import logo from './logo.svg';
 
+const SmallSquare = () =>  <div style={{ minWidth: '120px', height: '120px', backgroundColor: 'teal', margin: '10px'}}/>
+const BigSquare = () =>  <div style={{ minWidth: '240px', height: '240px', backgroundColor: 'red', margin: '10px'}}/>
+const WideSquare = () =>  <div style={{ minWidth: '240px', height: '120px', backgroundColor: 'green', margin: '10px'}}/>
+const VeryWideSquare = () =>  <div style={{ minWidth: '360px', height: '120px', backgroundColor: 'wheat', margin: '10px'}}/>
+
+
 export const LandingScreen = observer(() => {
 
     const state = useApplicationState();
@@ -13,14 +19,16 @@ export const LandingScreen = observer(() => {
         <div className="App">
         <header className="App-header">
           <img src={logo} className={`App-logo App-logo-${state.atomDirection}`} alt="logo" />
-          <div style={{ display: 'flex', flexDirection: 'row'}}>
+          <div style={{ display: 'flex', flexDirection: 'row', maxWidth: '60vw', border: '2px solid red', flexWrap: 'wrap'}}>
+          <SmallSquare />
+          <WideSquare />
+          <VeryWideSquare />
+          <BigSquare />
+          <SmallSquare />
+          </div>
+          <div>
           <Button label='Reverse' onClick={state.reverseAtom} />
           <ThemedButton label="Darken" onClick={state.button.darkenBackground} />
-          <div style={{ width: '250px', height: '120px', backgroundColor: 'teal', margin: '10px'}}/>
-          <div style={{ width: '250px', height: '120px', backgroundColor: 'teal', margin: '10px'}}/>
-          <div style={{ width: '250px', height: '120px', backgroundColor: 'teal', margin: '10px'}}/>
-          <div style={{ width: '250px', height: '120px', backgroundColor: 'teal', margin: '10px'}}/>
-
           <ThemedButton label="Thicken" onClick={state.button.thickenBorder} />
           </div>
         </header>
